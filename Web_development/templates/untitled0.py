0d3460729a2695b2f5a6eb45fb1caedcd7a82166
Kama17 <69436311+Kama17@users.noreply.github.com>
2022-07-18 06:50:04 +0100

    Delete Web_development directory

--- Web_development/templates/untitled0.py
+++
@@ -1,3 +0,0 @@
-        <a href="{{ url_for('edit', task_id=task.id) }}" class="btn btn-primary">Edit</a>
-        <a href="{{ url_for('delete', task_id=task.id) }}" class="btn btn-danger">Delete</a>
-
